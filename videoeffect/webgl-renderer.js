ef22f18412bd63572d9b7ea046fa783553fc40c2
Xu Xing <xing.xu@intel.com>
2025-09-12 11:42:20 +0800

    Remove duplicate texImage2D (#3)

--- videoeffect/webgl-renderer.js
+++ videoeffect/webgl-renderer.js
@@ -49,7 +49,6 @@ function renderWithWebGL2(gl, program, videoFrame, maskImageData, resources) {
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, videoFrame);
     
     // Update mask texture
     gl.bindTexture(gl.TEXTURE_2D, maskTexture);
